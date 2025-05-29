c35d7555fb9a042db97693e0eed28ae1c73c2842
Rylee Rosenlieb <ryleerosenlieb@Rylees-MacBook-Air.local>
2025-05-29 13:09:22 +0400

    update introductionPage formatting

--- src/pages/IntroductionPage.jsx
+++ src/pages/IntroductionPage.jsx
@@ -25,28 +25,54 @@ const IntroductionPage = () => {
         <Container>
           <div className="row">
             {/* Left column: Main Content */}
-            <div className="col-md-8">
+            <div className="col-md-7 offset-md-0"
+            style={{ marginLeft: "5%" }}>
               <section>
-                <h4 className="mt-4">WEBSITE Description</h4>
+                {/*<h4 className="mt-4">WEBSITE Description</h4>*/}
+                <h4
+                  style={{
+                    display: "inline-block",
+                    paddingBottom: "0.25rem",
+                    borderBottom: "3px solid var(--custom-blue)",
+                  }}
+                >
+                  WEBSITE DESCRIPTION
+                </h4>
                 <p>
                   OTSEC-HUB is a platform for the Operational Technology (OT) Security community. We provide content related to OT security for both academic and research purposes. Our goal is to offer talks, courses, interactive labs, datasets, and more—free for everyone. Anyone can collaborate and share their knowledge here!
                 </p>
 
-                <h4 className="mt-4">WEBSITE Objectives</h4>
+                <h4
+                  style={{
+                    display: "inline-block",
+                    paddingBottom: "0.25rem",
+                    borderBottom: "3px solid var(--custom-blue)",
+                  }}
+                  >WEBSITE OBJECTIVES</h4>
                 <ul>
                   <li>Becoming one of the largest hubs for OT security practitioners and enthusiasts.</li>
                   <li>Offering learning content through courses and interactive labs, covering topics from beginner to advanced levels.</li>
                   <li>Advancing OTSec research by collecting datasets, facilitating communication among researchers, and organizing monthly talks.</li>
                 </ul>
 
-                <h4 className="mt-4">What you will find here</h4>
+                <h4                   
+                  style={{
+                    display: "inline-block",
+                    paddingBottom: "0.25rem",
+                    borderBottom: "3px solid var(--custom-blue)",
+                  }}>WHAT YOU WILL FIND HERE</h4>
                 <ul>
                   <li><strong>Talks Page</strong> – Recordings of our monthly talks, featuring researchers and engineers discussing relevant topics.</li>
                   <li><strong>Resources Page</strong> – Links to courses, labs, research papers, and ready-to-use datasets.</li>
                   <li><strong>Contact Page</strong> – Instructions on how to become a permanent member by filling out a form.</li>
                 </ul>
 
-                <h4 className="mt-4">How to JOIN US?</h4>
+                <h4                   
+                  style={{
+                    display: "inline-block",
+                    paddingBottom: "0.25rem",
+                    borderBottom: "3px solid var(--custom-blue)",
+                  }}>HOW TO JOIN US?</h4>
                 <p>
                   If you’d like to contribute by offering a talk, course, dataset, or if you have a suggestion, feel free to contact us at <code>mm6446 at nyu dot edu</code>.
                 </p>
